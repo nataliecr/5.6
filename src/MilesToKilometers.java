3be0f181ade3840f0ad0ea799059de6dbbd9a7bc
nataliecr <pikayoshi21@gmail.com>
2016-03-07 13:23:32 -0800

    fixed spacing issues

--- src/MilesToKilometers.java
+++ src/MilesToKilometers.java
@@ -4,15 +4,19 @@ public class MilesToKilometers {
   public static void main(String[] args){
     int miles = 1;
     int kilometers2 = 20;
-    System.out.print("Miles         Kilometers | ");
-    System.out.println("Kilometers         Miles");
+    System.out.printf("%-10s%15s", "Miles", "Kilometers");
+    System.out.print(" | ");
+    System.out.printf("%-9s%15s\n", "Kilometers", "Miles");
     
-    while (miles < 11 && kilometers2 < 66){
+    while (miles <= 10 && kilometers2 <= 65){
       double kilometers = miles * 1.609;  
-      System.out.printf(miles + "%23.3f | "
-              , kilometers);
+      //System.out.printf(miles + "%24.3f | "
+              //, kilometers);
+      System.out.printf("%-5d%15.3f ", miles, kilometers);
+      System.out.print("     | ");
       double miles2 = kilometers2 / 1.609;
-      System.out.printf(kilometers2 + "%23.3f\n", miles2);
+      //System.out.printf(kilometers2 + "%24.3f\n", miles2);
+      System.out.printf("%-12d%14.3f\n", kilometers2, miles2);
       kilometers2 += 5;  
       miles++;
     } // while
